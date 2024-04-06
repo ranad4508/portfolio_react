65d614f91ad4f827e29e4cb1fbdebb6c5aa714ee
Dinesh Rana <ranad4508@gmail.com>
2024-04-06 07:01:21 +0545

    updated

---
+++ src/data/constants.js
@@ -0,0 +1,268 @@
+export const Bio = {
+  name: "Dinesh Kumar Rana",
+  roles: [
+    "Full Stack Developer",
+    "Android Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/ranad4508",
+  resume:
+    "https://drive.google.com/file/d/1MWAZKco0A-VIzIpiZINw165E820zHc-K/view",
+  linkedin: "https://www.linkedin.com/in/dinesh-rana-616a18225/",
+  twitter: "https://twitter.com/dinesh4508",
+  insta: "https://www.instagram.com/_iam_krypton/",
+  facebook: "https://www.facebook.com/dineshDevops",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML5",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS 3",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "TypeScript",
+        image: "https://cdn.worldvectorlogo.com/logos/typescript-2.svg",
+      },
+      {
+        name: "jQuery",
+        image:
+          "https://cdn.iconscout.com/icon/free/png-256/free-jquery-3521520-2945023.png?f=webp",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "PHP",
+        image: "https://www.php.net/images/logos/new-php-logo.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "DotNet",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/e/ee/.NET_Core_Logo.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "XML",
+        image: "https://www.svgrepo.com/show/31053/xml.svg",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.svg",
+      },
+      {
+        name: "C",
+        image: "https://cdn.worldvectorlogo.com/logos/c-1.svg",
+      },
+      {
+        name: "C++",
+        image: "https://cdn.worldvectorlogo.com/logos/c.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Github-desktop-logo-symbol.svg/2048px-Github-desktop-logo-symbol.svg.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/1667px-Figma-logo.svg.png",
+      },
+      {
+        name: "Atlassian Jira",
+        image:
+          "https://cdn.icon-icons.com/icons2/2699/PNG/512/atlassian_jira_logo_icon_170511.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbZYktaNgd9uqoFzruELxMIAqMdwcp0VMBIrmBCgALWkUiAMMnULY1KqByCEzvPM6rDn4&usqp=CAU",
+    role: "Professional IT Course on Frontend/Web Designing",
+    company: "School of Information Technologies",
+    date: "Jan 2022 - April 2022",
+    desc: "Completed the course learning about the web designing held by the School of Information Technologies.",
+    skills: ["HTML5", "CSS 3", "Bootstrap", "JavaScript", "jQuery"],
+    // doc: "https://drive.google.com/drive/u/0/folders/1IK1e_3HlIEV6S6tekweL0bw7V4CHf5gD",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.edusanjal.com/__sized__/logos/Orchid_Logo-thumbnail-200x200-70.jpg",
+    school: "Orchid International College Bijayachowk, Gaushala, Kathmandu",
+    date: "Apr 2020 - Ongoing",
+    grade: "3.50",
+    desc: "I am currently pursuing a Bachelor's degree in Bachelor in Computer Application at Orchid International College Bijayachowk, Gaushala, Kathmandu. I have completed 4 semesters and have a CGPA of 3.50. I have taken courses in Data Structures and Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "BCA - Bachelor in Computer Application",
+  },
+  {
+    id: 1,
+    img: "https://play-lh.googleusercontent.com/bZbJLqJkBiwKm3J9Erdn2LTtsBXRSUslngLKn7Bh2z8MmVtwvozDfOzLIqBJZQnng-ae",
+    school: "NASA International College Tinkune, Gairigaun, Kathmandu",
+    date: "Apr 2018 - Dec 2020",
+    grade: "2.90",
+    desc: "I completed my class 12 high school education at NASA International College Tinkune, Gairigaun, Kathmandu, where I studied Science with Computer Science.",
+    degree: "Intermediate Education, Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://lh3.googleusercontent.com/p/AF1QipOQ9guqmaQpPg8ix0IqXJ1n0iuX0hOhN2arkIhy=s1360-w1360-h1020",
+    school: "Soojung English Boarding School, Rajpur, Doti",
+    date: "Apr 2006 - Apr 2018",
+    grade: "3.55",
+    desc: "I completed my class 10 education at Soojung English Boarding School, Rajpur, Doti.",
+    degree: "SEE - Secondary Education Examination",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "NextGen",
+    date: "Jan 2023 - May 2023",
+    description:
+      "Developed a full-stack web application that allows users to buy courses and learn them with secure payment gateway. Learner can buy course and start learning them tutor can post course and reply learner's queries and admin can manage courses, tutors and learners.",
+    image:
+      "https://raw.githubusercontent.com/ranad4508/ProjectI_NextGen/main/nextgen.png",
+    tags: ["HTML5", "CSS3", "PHP", "jQuery", "JavaScript", "Ajax", "Bootstrap"],
+    category: "web app",
+    github: "https://github.com/ranad4508/ProjectI_NextGen",
+    webapp: "https://github.com/ranad4508/ProjectI_NextGen",
+    member: [
+      {
+        name: "Dinesh Kumar Rana",
+        img: "https://avatars.githubusercontent.com/u/97509734?v=4",
+        linkedin: "https://www.linkedin.com/in/dinesh-rana-616a18225/",
+        github: "https://github.com/ranad4508",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "GadgetHub",
+    date: "Dec 2023 - Feb 2024",
+    description:
+      "Designed and developed full stack ecommerce web application that allows users to buy gadgets using secure payment gateway. User can add item in cart, checkout and order item where admin manages all the stuffs.",
+    image:
+      "https://raw.githubusercontent.com/ranad4508/GadgetHub/main/gadgethub.png",
+    tags: ["HTML5", "CSS3", "PHP", "jQuery", "JavaScript", "Ajax", "Bootstrap"],
+    category: "web app",
+    github: "https://github.com/ranad4508/GadgetHub",
+    webapp: "https://github.com/ranad4508/GadgetHub",
+  },
+  {
+    id: 2,
+    title: "GPT-3",
+    date: "Jan 2022 - Feb 2022",
+    description:
+      "Developed the web application using react js which shows the dominant of AI in todays world. It shows the environment of ChatGPT-3.",
+    image:
+      "https://raw.githubusercontent.com/ranad4508/GPT-3-using-react/main/gpt-3.png",
+    tags: ["ReactJS", "JavaScript"],
+    category: "web app",
+    github: "https://github.com/ranad4508/GPT-3-using-react",
+    webapp: "https://dineshgpt-3.netlify.app/",
+  },
+  {
+    id: 10,
+    title: "Nika Helmet",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Designed and developed the web application for online helmet store where user can buy helmet, accessories and etc. It shows the latest helmets and price of the helmets where can choose according to their needs.",
+    image:
+      "https://raw.githubusercontent.com/ranad4508/nika-helmet/main/nika-helmet.png",
+    tags: ["HTML5", "CSS3", "Bootstrap", "JavaScript", "jQuery"],
+    category: "web app",
+    github: "https://github.com/ranad4508/nika-helmet",
+    webapp: "https://nikahelmet.netlify.app/",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2020, text: "Started my Bachelor Education" },
+  {
+    year: 2021,
+    text: "Completed course from school of information technology",
+  },
+  { year: 2022, text: "Started building web applications" },
+];
